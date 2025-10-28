4b758564a86fe275425672c77c4c7b8c6a5dad89
MaidHalilovic <maidhaliloviclimunada567@gmail.com>
2025-10-28 22:43:18 +0100

    started gym-app project

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Maid najjaci programer na svetu</Text>
+      <Text>67</Text>
+    </View>
+  );
+}
