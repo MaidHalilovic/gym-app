5509e457b1bf051366a07dca7859fe23398122fa
MaidHalilovic <maidhaliloviclimunada567@gmail.com>
2025-11-02 18:35:25 +0100

    Made a Header

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,11 +1,49 @@
-import { Image, View } from "react-native";
-import { Text } from "react-native-gesture-handler";
+import { Image, StyleSheet, TextInput, View } from "react-native";
 
 const Header = () => {
+  const styles = StyleSheet.create({
+    header: {
+      backgroundColor: "#BB6653",
+      display: "flex",
+      // alignItems: "center",
+      justifyContent: "center",
+      flexDirection: "row",
+      gap: 70,
+      height: 180,
+    },
+    searchBox: {
+      backgroundColor: "#FFF8E8",
+      width: 200,
+      height: 50,
+      borderRadius: 10,
+      marginTop: 90,
+    },
+    input: {
+      padding: 10,
+      fontSize: 18,
+    },
+  });
+
   return (
-    <View>
-      <Text>Header</Text>
-      <Image source={require("../../../assets/images/gym-logo.png")} />
+    <View style={styles.header}>
+      <View>
+        <Image
+          source={require("../../../assets/images/gympic.png")}
+          style={{
+            marginTop: 70,
+            marginLeft: 20,
+            width: 80,
+            height: 80,
+          }}
+        />
+      </View>
+      <View style={styles.searchBox}>
+        <TextInput
+          placeholder='Search'
+          placeholderTextColor={"black"}
+          style={styles.input}
+        />
+      </View>
     </View>
   );
 };
