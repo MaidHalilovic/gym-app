740978128160bb6b5fd704279885aa113170fc6b
Maid Halilovic <maidhaliloviclimunada567@gmail.com>
2025-11-29 19:24:36 +0100

    Making function for tracking on calendaer

--- app/Tracking/VisitsPerMonth.tsx
+++ app/Tracking/VisitsPerMonth.tsx
@@ -1,16 +1,25 @@
 import { useRouter } from "expo-router";
 import { useState } from "react";
-import { Button, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 import { Calendar } from "react-native-calendars";
 
 const VisitsPerMonth = () => {
   const [selectedDate, setselectedDate] = useState("");
 
+  const Styles = StyleSheet.create({
+    container: {
+      display: "flex",
+    },
+  });
+
   const router = useRouter();
   return (
-    <View>
+    <View style={Styles.container}>
       <Text>Calendar</Text>
       <Button title='Go back' onPress={() => router.back()} />
+      {/* <View style={Styles.btn}>
+        <Button title='Add +1 Visit' color={""} />
+      </View> */}
       <Calendar
         onDayPress={(day) => {
           setselectedDate(day.dateString);
