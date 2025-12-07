c13178e4534b21e2e4df7197bf8ed22a1607d480
Maid Halilovic <maidhaliloviclimunada567@gmail.com>
2025-12-07 20:41:41 +0100

    Made two logo that is not working for now (have to make  profile page and login form)

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,20 +1,31 @@
+import { Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
 import { Image, StyleSheet, View } from "react-native";
 
 const Header = () => {
+  const router = useRouter();
+
   const styles = StyleSheet.create({
     header: {
       backgroundColor: "#007E6E",
       display: "flex",
       justifyContent: "center",
       flexDirection: "row",
-      gap: 80,
+      gap: 120,
       height: 180,
     },
     input: {
       padding: 10,
       fontSize: 18,
     },
-  }); //need to style header (remove the search and add the button for search and add profile button for progile
+    icons: {
+      display: "flex",
+      flexDirection: "row",
+      gap: 50,
+      marginTop: 100,
+      height: 40,
+    },
+  }); //need to style header (remove the search and add tfontSize: 24,he button for search and add profile button for progile
 
   return (
     <View style={styles.header}>
@@ -29,8 +40,18 @@ const Header = () => {
           }}
         />
       </View>
-      <View></View>
+      <View style={styles.icons}>
+        <Ionicons name='search' color={"white"} size={35} />
+        //search button
+        <Ionicons
+          name='person'
+          color={"white"}
+          size={35}
+          onPress={() => router.push("/")} //profile button
+        />
+      </View>
     </View>
+    //need to make profile button functional when login form and profile page are created
   );
 };
 
